08bffab2801a6cfa317591e58bce81140d2e9c3c
Varabei Kanstantsin <vorobeybird@gmail.com>
2022-03-20 15:48:03 +0500

    add formatting

--- src/app/toggler/toggler.component.ts
+++ src/app/toggler/toggler.component.ts
@@ -3,25 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-toggler',
   templateUrl: './toggler.component.html',
-  styleUrls: ['./toggler.component.css']
+  styleUrls: ['./toggler.component.css'],
 })
 export class TogglerComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   public isActive = true;
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggleActive() {
+    this.isActive = !this.isActive;
   }
 
-  toggleActive(){
-    this.isActive = !this.isActive
+  get statusLabel(): string {
+    return this.isActive ? 'Active' : 'Inactive';
   }
-
-  get statusLabel():string{
-
-    return this.isActive ? 'Active' : 'Inactive'
-
-  }
-
 }
