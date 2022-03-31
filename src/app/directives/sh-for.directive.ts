bd7eb30d4a40ba39798769c0cfdb8dc769e64f42
Varabei Kanstantsin <vorobeybird@gmail.com>
2022-03-31 11:33:06 +0500

    fix formatting

--- src/app/directives/sh-for.directive.ts
+++ src/app/directives/sh-for.directive.ts
@@ -1,29 +1,20 @@
-import {
-  Directive,
-  Input,
-  TemplateRef,
-  ViewContainerRef,
-} from '@angular/core'
-
-
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
-  selector: '[shFor]'
+  selector: '[shFor]',
 })
 export class ShForDirective {
-
   constructor(
     private templateRef: TemplateRef<any>,
     private viewContainer: ViewContainerRef
   ) {}
 
-
   @Input()
   set shFor(times: number) {
-    for (let i = 0; i < times; i++) {
-      this.viewContainer.createEmbeddedView(this.templateRef,{
-        $implicit: i
-      });
+    for (let index = 0; index < times; index++) {
+      this.viewContainer.createEmbeddedView(this.templateRef, 
+        {$implicit: index}
+        );
     }
   }
 }
